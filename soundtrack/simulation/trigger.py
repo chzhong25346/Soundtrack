26efb5f0ce994fe614bb85252009a2aa5b76c555
Colin Zhong <colin25346@gmail.com>
2019-10-28 20:51:39 -0400

    new buy strategy

--- soundtrack/simulation/trigger.py
+++ soundtrack/simulation/trigger.py
@@ -10,11 +10,11 @@ def buy_strategy_a(df):
     #     pass
     pass
 
-
+# new update strategy Oct 25 2019
 def buy_strategy_b(df):
     try:
-        return df[(df['yr_high'] == 1) & (df['yr_low'] == 0) & (df['downtrend'] == 0) & (df['macd'] == '0') &
-                (df['rsi'] == '0') & (df['high_volume'] == 1) & (df['volume_price'] == 1)].index.tolist()
+        return df[(df['yr_high'] == 1) & (df['downtrend'] == 0) & (df['uptrend'] == 1) &(df['macd'] == 'buy') &
+                (df['high_volume'] == 1)].index.tolist()
     except Exception as e:
         logger.debug('bull_hivolume_uptrend: Missing Field in Report for Calculation!')
         pass
