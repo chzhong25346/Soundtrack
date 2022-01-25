75a94fe77efedc5b29d818f598244f6871ba4965
colin <colin25346@gmail.com>
2022-01-25 09:32:00 -0500

    update

--- soundtrack/utils/fetch.py
+++ soundtrack/utils/fetch.py
@@ -114,7 +114,7 @@ def get_tmxmoney_daily(ticker):
 
 
 def get_yahoo_finance_price(ticker):
-    time.sleep(2)
+    time.sleep(5)
     url = 'https://finance.yahoo.com/quote/'+ticker+'/history?p='+ticker
     headers = {"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
 "accept-encoding": "gzip, deflate, br",
